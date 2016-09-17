63821390fa79310adbbfed16ecb26082731e7858
Nick <tyr.arcana@gmail.com>
2016-09-17 18:22:52 +0100

    Basic scripts for player movement, spawning, and death

---
+++ GDL_AI_Jam/Assets/Scripts/KillOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillOnCollision : MonoBehaviour
+{
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        Player other = otherCollider.GetComponent<Player>();
+        if (other != null)
+            KillPlayer(other);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        for (int i=0; i<collision.contacts.Length; i++)
+        {
+            Player other = collision.contacts[0].otherCollider.GetComponent<Player>();
+            if (other != null)
+                KillPlayer(other);
+        }
+    }
+
+    void KillPlayer(Player player)
+    {
+        player.Kill();
+    }
+
+    Collider _collider;
+}
---
+++ GDL_AI_Jam/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public Text[] SpawnLabels;
+    public Player PlayerPrefab;
+    public bool CanRespawn = false;
+
+    void Start()
+    {
+        _players = new Player[4];
+        for (int i = 1; i < 4; i++)
+        {
+            ShowSpawn(i);
+            _players[i] = null;
+        }
+    }
+
+    void Update()
+    {
+        //check for absent players pushing A
+        for (int i = 1; i < 4; i++)
+        {
+            if (_players[i] == null)
+            {
+                if (Input.GetAxisRaw("A_" + i) > 0.5)
+                {
+                    SpawnPlayer(i);
+                    ClearMessage(i);
+                }
+            }
+        }
+    }
+
+    void SpawnPlayer(int i)
+    {
+        GameObject playerGO = (GameObject)Instantiate(PlayerPrefab.gameObject, transform.position, Quaternion.identity);
+        _players[i] = playerGO.GetComponent<Player>();
+        _players[i].ControllerNumber = i;
+    }
+
+    public void PlayerKilled(Player player)
+    {
+        for (int i = 1; i < 4; i++)
+            if (_players[i] == player)
+            {
+                if (CanRespawn)
+                {
+                    ShowReSpawn(i);
+                    _players[i] = null;
+                    Destroy(player.gameObject);
+                }
+                else
+                    player.CanMove = false;
+            }
+    }
+
+    void ShowSpawn(int i)
+    {
+        SpawnLabels[i].text = i + ": Press A to enter level";
+    }
+
+    void ShowReSpawn(int i)
+    {
+        SpawnLabels[i].text = i + ": Press A to respawn";
+    }
+
+    void ClearMessage(int i)
+    {
+        SpawnLabels[i].text = "";
+    }
+
+    Player[] _players;
+}
