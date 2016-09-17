3598414da2db5bfe050ba2a90efbfb582cb68a57
Nick <tyr.arcana@gmail.com>
2016-09-17 22:56:11 +0100

    Adding Trapdoor and triggerable trap base class

---
+++ GDL_AI_Jam/Assets/Scripts/TriggerableTrap.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerableTrap : MonoBehaviour
+{
+    [Tooltip("A, B, X, Y, LB or RB")]
+    public string TriggerButtonID;
+
+    public float CooldownSeconds = 4;
+    public float ActivationTime = 2;
+
+    void Update()
+    {
+        if (_cooldown > 0)
+        {
+            _cooldown -= Time.deltaTime;
+            if (_cooldown <= 0)
+                ReEnable();
+        }
+
+        if (_active > 0)
+        {
+            _active -= Time.deltaTime;
+            if (_active <= 0)
+            {
+                ToggleState(false);
+            }
+        }
+        else if (_cooldown <= 0)
+        {
+            //poll controller
+            if (Input.GetAxisRaw(TriggerButtonID + "_1") > 0.5)
+            {
+                _active = ActivationTime;
+                _cooldown = CooldownSeconds;
+                ToggleState(true);
+            }
+        }
+    }
+
+    public virtual void ToggleState(bool active)
+    {
+    }
+
+    public virtual void ReEnable()
+    {
+    }
+
+    protected float _cooldown = 0;
+    protected float _active = 0;
+}
