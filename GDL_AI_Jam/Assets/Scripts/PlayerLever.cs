419dc740a561e089811f90863a2c1124ab4c2a85
Nick <tyr.arcana@gmail.com>
2016-09-18 16:25:36 +0100

    Levers and their interaction with doors

---
+++ GDL_AI_Jam/Assets/Scripts/PlayerLever.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerLever : MonoBehaviour
+{
+    public int LeverID;
+
+    void Start()
+    {
+        Door[] doors = FindObjectsOfType<Door>();
+        _matchingDoors = new List<Door>();
+        foreach (Door door in doors)
+        {
+            if (door.LeverType == LeverID)
+            {
+                _matchingDoors.Add(door);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        Player other = otherCollider.GetComponent<Player>();
+        if (other != null)
+            OpenDoors(other);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            Player other = collision.contacts[0].otherCollider.GetComponent<Player>();
+            if (other != null)
+                OpenDoors(other);
+        }
+    }
+
+    void OpenDoors(Player player)
+    {
+        foreach (Door door in _matchingDoors)
+            door.Open(LeverID);
+    }
+
+    List<Door> _matchingDoors;
+}
