044e9a5024c809352268df08eaa37f4a3f410ba0
H-PC\H <hannesdelbeke@gmail.com>
2016-09-18 16:31:12 +0100

    hannes

---
+++ GDL_AI_Jam/Assets/Scripts/MovementTrapSpike.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementTrapSpike : TriggerableTrap
+{
+	public float MoveOffset;
+    public float MovementSpeed;
+	public GameObject particles;
+
+    void Start()
+    {
+		_startPos = transform.localPosition;
+    }
+
+    override public void ToggleState(bool active)
+    {
+        Debug.Log((active ? "Activating" : "Deactivating") + " Sliding Trap on " + TriggerButtonID);
+		if (particles)
+		particles.SetActive (active);
+		StartCoroutine(Slide(active ? MoveOffset : 0 ));
+    }
+
+    IEnumerator Slide(float moveOffset)
+    {
+		Vector3 fromPos = transform.localPosition;
+        for (float t = 0; t < 1; t += Time.deltaTime / MovementSpeed)
+        {
+			transform.localPosition = Vector3.Lerp(fromPos,_startPos + transform.up*moveOffset, t);
+            yield return null;
+        }
+		//if(moveOffset == 0 )
+		//transform.localPosition = _startPos;
+    }
+
+    Vector3 _startPos;
+}
